dead3ac5e5a9cdcd8ade7e043e442505df3b2706
tribeiros <tribeiros@indracompany.com>
2017-10-24 17:08:20 -0200

    first

---
+++ library/sseg.php
@@ -0,0 +1,155 @@
+<HTML>
+<HEAD><TITLE>Sigils, Servitors, Egregores, and Godforms - [ChaosMatrix.org]</TITLE>
+<LINK REL="stylesheet" TYPE="text/css" HREF="/chaosmatrix.css">
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38176002-1']);
+  _gaq.push(['_setDomainName', 'chaosmatrix.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+</HEAD>
+
+<BODY BGCOLOR="#FFFFFF" BACKGROUND="/bg/matrixbg.jpg" TEXT="#000000" LINK="#660099" VLINK="#990099" ALINK="#990099">
+
+
+<!--CENTER-->
+
+
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="100%"><!--
+	--><TR><TD COLSPAN="2"><!--
+		--><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="100%"><TR><!--
+			--><TD VALIGN="top" ALIGN="left"><a href="/"><IMG SRC="/img/matrix.jpg" ALT="[ C h a o s   M a t r i x ]" BORDER="0" VSPACE="5"></a></TD><!--
+			--><TD VALIGN="center" ALIGN="right"><IMG SRC="/img/children.gif"></TD><!--
+			--></TR><!--
+		--></TABLE><!--
+	--></TD></TR><!--
+	--><TR><!--
+--><!---------------------------------------------------------------------------
+   SIDE NAVIGATION
+---------------------------------------------------------------------------><!--
+		--><TD COLSPAN="1" ALIGN="left" VALIGN="top"><!--
+			--><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="100" BGCOLOR="#FFFFFF"><!--
+				--><TR><!--
+					--><TD><IMG SRC="/img/borders/blackline/h.gif" WIDTH="9" HEIGHT="9" ALT="" ALIGN="right"></TD><!--
+					--><TD BACKGROUND="/img/borders/blackline/h.gif" ALT=""><IMG SRC="/img/dots/clear.gif" WIDTH="1" HEIGHT="1" ALT=""></TD><!--
+					--><TD><IMG SRC="/img/borders/blackline/t1.gif" WIDTH="9" HEIGHT="9" ALT=""></TD><!--
+					--><TD BACKGROUND="/img/borders/blackline/h.gif" ALT=""><IMG SRC="/img/dots/clear.gif" WIDTH="5" HEIGHT="1" ALT=""></TD><!--
+				--></TR><!--
+				--><TR><!--
+					--><TD BACKGROUND="/img/borders/blackline/clear.gif"><IMG SRC="/img/dots/clear.gif" WIDTH="1" HEIGHT="1" ALT=""></TD><!--
+					--><TD>
+<FONT FACE="Arial, Verdana, Helvetica, sans-serif" SIZE="1">
+<NOBR>
+<FONT CLASS="sidenav">
+<A HREF="/index.php">Home</A><BR><A HREF="/library/chaos.php">Chaos&nbsp;Magick</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/chaos_all.php">All&nbsp;Chaos&nbsp;Magick&nbsp;Files</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/whatischaos.php">What&nbsp;is&nbsp;Chaos&nbsp;Magick?</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/rites.php">Rituals,&nbsp;Workings,&nbsp;Spells,&nbsp;Techniques</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/sseg.php">Sigils,&nbsp;Servitors,&nbsp;Egregores,&nbsp;Godforms</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/gnosis.php">Gnosis</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/spare.php">Austin&nbsp;Osman&nbsp;Spare&nbsp;(writing&nbsp;and&nbsp;art)</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/cthulhu.php">Cthulhu/Necronomicon</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/ob.php">Ouranian&nbsp;Barbaric</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/chaosscience.php">Chaos&nbsp;Science</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/topy.php">Temple&nbsp;ov&nbsp;Psychick&nbsp;Youth&nbsp;(TOPY)</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/carroll.php">Peter&nbsp;Carroll/IOT</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/ud.php">Frater&nbsp;U:.D:.</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/discord.php">Discordianism</A><BR>&nbsp;&nbsp;&nbsp;<A HREF="/library/chaosmisc.php">Chaos&nbsp;Magick&nbsp;Miscellany</A><BR><A HREF="/library/magick.php">Other&nbsp;Magick</A><BR><A HREF="/library/humor.php">Occult&nbsp;Humor</A><BR><A HREF="/library/books.php">Digital&nbsp;Books</A><BR></FONT>
+</NOBR>
+</FONT>
+						</TD><!--
+					--><TD BACKGROUND="/img/borders/blackline/v.gif" WIDTH="9" HEIGHT="9"><IMG SRC="/img/dots/clear.gif" WIDTH="1" HEIGHT="1" ALT=""></TD><!--
+				--></TR><!--
+				--><TR><!--
+					--><TD><IMG SRC="/img/borders/blackline/h.gif" HEIGHT="9" ALT="" ALIGN="right"></TD><!--
+					--><TD BACKGROUND="/img/borders/blackline/h.gif" ALT=""><IMG SRC="/img/dots/clear.gif" WIDTH="1" HEIGHT="1" ALT=""></TD><!--
+					--><TD><IMG SRC="/img/borders/blackline/lr.gif" WIDTH="9" HEIGHT="9" ALT=""></TD><!--
+				--></TR><!--
+
+			--></TABLE><!--
+		--></TD><!--
+
+
+
+--><!---------------------------------------------------------------------------
+   MAIN BODY
+---------------------------------------------------------------------------><!--
+		--><TD ALIGN="left" VALIGN="top" WIDTH="100%"><!--
+			--><TABLE WIDTH="100%" BORDER="0" CELLSPACING="0" CELLPADDING="0"><!--
+				--><TR><TD WIDTH="100%" ALT=""><IMG SRC="/img/dots/clear.gif" WIDTH="1" HEIGHT="4" ALT=""></TD></TR><!--
+				--><TR><TD WIDTH="100%" BACKGROUND="/img/dots/black.gif" ALT=""><IMG SRC="/img/dots/clear.gif" WIDTH="1" HEIGHT="1" ALT=""></TD></TR><!--
+				--><TR><!--
+					--><TD ALIGN="left" VALIGN="top" BGCOLOR="#FFFFFF">
+
+
+
+
+<FONT CLASS="head2">Sigils, Servitors, Egregores, and Godforms</FONT><BR><BR>
+
+
+<TABLE COLS=3 WIDTH=100% CELLSPACING=0 CELLPADDING=0 BORDER=0>
+ <TR><TD WIDTH=""><IMG SRC="/img/dots/clear.gif" WIDTH="5" HEIGHT=1 ALT=""></TD>
+     <TD WIDTH=""><IMG SRC="/img/dots/clear.gif" WIDTH="16" HEIGHT=1 ALT=""></TD>
+     <TD WIDTH="100%"></TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/acoustic.html">Acoustic Sigils</A>, A method for making musical or acoustic sigils</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/actsig.html">Action Sigils</A>, Ray Sherwin's method for sigilisation</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/apikindx.html">Apikorsus</A>, An essay on the diverse practices of Chaos Magick</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/spare/aosig.html">Austin Osman Spare and his Theory of Sigils</A>, Frater U:.D:. demonstrates Austin Spare's methods of sigilisation</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/szord.html">Baphomet Dual Sigil Rave Invocation</A>, Hurling invocations between two participants</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/spare/pleasure.html">The Book of Pleasure (Self Love) -- The Psychology of Ecstasy</A>, Spare's Magick--- sigils, the alphabet of desire, and more</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/demonet.html">Demons in the 'Net</A>, An explanation of the famed 1994 TIAMAT working to banish demons from the internet---and the demons' retaliation</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/fotamec2.html">Don't Blame Me---Blame My Servitor</A>, A history of the creation and evolution of the Fotamecus Time Manipulation Spirit</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/egregore.html">Egregore</A>, Notes on the role of the historical egregore in modern magick</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/gegregor.html">Egregore Definition Compilation</A>, A compilation of various definitions of an egregore</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/confused.html">Fireclown's Sigilization Basics for the Confused</A>, Sigils got you confused? Sort it out with this short essay.</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/fen-egre.html">The Fluid Continuum --or-- What the F***'s an Egregore?</A>, The relationship between sigils, servitors, egregores, and godforms and their logical progression</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/rites/fotamec3.html">Fotamecus Empowerment Rite</A>, The ritual that empowered Fotamecus to the point of egregore-hood</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/fotamec1.html">Fotamecus: Viral Time Compression/Expansion Servitor</A>, The original Fotamecus Time Manipulation essay</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/frank.html">The Living Servitor</A>, How to take care of your servitors</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/orchaos.html">Oven-Ready Chaos</A>, Originally the "Condensed Chaos" chapbook that was the precursor to the book of the same name</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/santa.txt">The Santa Egregore</A>, Contrary to popular belief, Santa Claus is quite real...</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/servitor.txt">Servitor Creation: A Contemporary Approach</A>, Fleshing out and empowering a skeletal sigil to create a servitor</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/sigsergod.txt">Sigils, Servitors, and Godforms, Part I: Sigils</A>, Part I (sigils) of Marik's insightful essay</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/servitors.html">Sigils, Servitors, and Godforms, Part II: Servitors</A>, Part II (servitors) of Marik's insightful essay</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/smuggle.txt">Smuggling Sigils Across</A>, Casting sigils for clients</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/rites/fotamec4.html">Time Consumption -or- Yet Another Fotamecus Rite</A>, Drawing the power of Fotamecus into yourself through kitchen magick</TD></TR>
+ <TR><TD></TD><TD VALIGN=top ALIGN=center><IMG SRC="/img/buttons/bl.gif" VSPACE=5 ALT="* "></TD><TD>
+  <A HREF="/library/chaos/texts/zserv.txt">A Z(cluster) Servitor Thread</A>, An early z-list thread about servitors</TD></TR>
+</TABLE>
+
+
+
+
+
+
+<BR>
+<FONT SIZE="1" COLOR="#FFFFFF">[0]</FONT>
+					</TD><!--
+				--></TR><!--
+			--></TABLE><!--
+		--></TD><!--
+	--></TR><!--
+--></TABLE>
+</BODY>
+</HTML>
